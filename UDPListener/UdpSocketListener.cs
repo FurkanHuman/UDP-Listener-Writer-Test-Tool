b7c00e88c58b444397ab94a793f0fc7aa5a7886c
Furkan Bozkurt <furkan-b136@hotmail.com>
2021-10-19 23:22:58 +0300

    add any code and time

--- UDPListener/UdpSocketListener.cs
+++ UDPListener/UdpSocketListener.cs
@@ -14,7 +14,12 @@ namespace UDPListener
             while (true)
             {
                 byte[] receivedData = udpClient.Receive(ref iPEndPoint);
-                Console.WriteLine(Encoding.ASCII.GetString(receivedData));
+
+                Console.WriteLine("Time: " + DateTime.Now + ":" + DateTime.Now.Millisecond + " | Hex: " + BitConverter.ToString(receivedData) + " | ASCII: " +
+                Encoding.ASCII.GetString(receivedData) + " | UTF8: " + Encoding.UTF8.GetString(receivedData) + " | UTF32: " +
+                Encoding.UTF32.GetString(receivedData) + " | Unicode: " + Encoding.Unicode.GetString(receivedData) + " | Latin1: " +
+                Encoding.Latin1.GetString(receivedData));
+
             }
         }
     }
